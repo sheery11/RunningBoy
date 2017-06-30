1a775a88be5869e76398a2a7a9b90aab87624a08
sheery11 <806161245@qq,com>
2017-06-30 18:33:06 +0800

    Loading

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # RunningBoy
-A project of GameDesign
+A project of GameDesign\n
+基于Unity3D开发的类似于QQ音速+酷跑的简单网络游戏\n
+在完善中
---
+++ Running_/Assets/City_pack/Script/ControlMove.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ControlMove : MonoBehaviour {
+	float speed = 3.5f;
+	private Animator anim;
+	// Use this for initialization
+	void Start () {
+		anim = GetComponent<Animator> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		if (Input.GetKey (KeyCode.UpArrow)) {
+			transform.Translate (Vector3.forward *speed*Time.deltaTime);
+		}
+		if (Input.GetKey (KeyCode.RightArrow)) {
+			anim.SetBool ("ifTurnRight",true);
+		}
+
+	}
+}
